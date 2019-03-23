16f089e0f53cc010a7e8ca1ebf94837b9b2d73f7
Jago <jago84@gmail.com>
2019-03-23 15:55:05 +0100

    App funcionando

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
+  providers: [Flashlight],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  showButton = false;
 
+  constructor(
+    private flash: Flashlight,
+    public platform: Platform
+  ) {}
+
+  async ngOnInit() {
+    await this.platform.ready();
+    this.checkShowButton();
+    this.checkBackButton();
+  }
+
+  async checkShowButton() {
+    this.showButton = this.isMobile() && await this.hasFlash();
+
+    if (this.showButton) {
+      this.platform.pause.subscribe(() => {
+        this.flash.switchOff();
+      });
+    }
+  }
+
+  private isMobile() {
+    return !!this.platform.platforms().find(platform =>
+      ['mobile', 'tablet', 'phablet'].includes(platform));
+  }
+
+  private async hasFlash() {
+    return await this.flash.available();
+  }
+
+  toggleFlash() {
+    this.flash.toggle();
+  }
+
+  private checkBackButton() {
+    this.platform.backButton.subscribe(() => {
+      // Comprobar navegación ??
+      // Salir de la app ??
+    });
+  }
+
+  ngOnDestroy() {
+    this.flash.switchOff();
+  }
 }
